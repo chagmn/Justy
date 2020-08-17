dfd899237845c9348e66987adfe91796166c6884
chagmn <sopg4@naver.com>
2020-08-17 18:43:21 +0900

    Initial Commit

---
+++ Justy-UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Justy-UIKit
+//
+//  Created by 창민 on 2020/08/17.
+//  Copyright © 2020 창민. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
