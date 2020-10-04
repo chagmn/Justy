ae5da8ef40e226b0adfe71b529dd400b9272da0f
chagmn <sopg4@naver.com>
2020-10-04 18:11:24 +0900

    add AddCustomerView4

---
+++ Justy-UIKit/AddCustomer4.swift
@@ -0,0 +1,18 @@
+//
+//  AddCustomer4.swift
+//  Justy-UIKit
+//
+//  Created by 창민 on 2020/10/04.
+//  Copyright © 2020 창민. All rights reserved.
+//
+
+import UIKit
+
+class AddCustomer4: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+        
+    }
+}
