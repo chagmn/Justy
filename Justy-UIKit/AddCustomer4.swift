52be23e6d97260b5b499f0be4014c234196fffb7
chagmn <sopg4@naver.com>
2020-10-05 20:02:56 +0900

    update AddCustomer4

--- Justy-UIKit/AddCustomer4.swift
+++ Justy-UIKit/AddCustomer4.swift
@@ -55,14 +55,15 @@ class AddCustomer4: UIViewController{
         childInfo_txt1.text = "자식 정보 :"
         childInfo_txt1.textAlignment = .center
         childInfo_txt1.textColor = .black
+        childInfo_txt1.backgroundColor = .yellow
         
-        childInfo_boy.text = "남"
-        childInfo_boy.textAlignment = .center
-        childInfo_boy.textColor = .black
+        childInfo_boy_txt.text = "남"
+        childInfo_boy_txt.textAlignment = .center
+        childInfo_boy_txt.textColor = .black
         
-        childInfo_girl.text = "여"
-        childInfo_girl.textAlignment = .center
-        childInfo_girl.textColor = .black
+        childInfo_girl_txt.text = "여"
+        childInfo_girl_txt.textAlignment = .center
+        childInfo_girl_txt.textColor = .black
     }
     
     func autoLayout(){
@@ -84,17 +85,19 @@ class AddCustomer4: UIViewController{
         micBtn.heightAnchor.constraint(equalToConstant: 35).isActive = true
         
         textField_AttestedCopy.translatesAutoresizingMaskIntoConstraints = false
-        textField_AttestedCopy.topAnchor.constraint(equalTo: micBtn.bottomAnchor, constant: 20).isActive = true
-        textField_AttestedCopy.trailingAnchor.constraint(equalTo: Btn_AttestedCopy.leadingAnchor, constant: -20).isActive = true
-        textField_AttestedCopy.widthAnchor.constraint(equalTo: view.widthAnchor, constant: (screenWidth-300)).isActive = true
+        textField_AttestedCopy.topAnchor.constraint(equalTo: micBtn.bottomAnchor, constant: 50).isActive = true
+        textField_AttestedCopy.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: 50).isActive = true
+        textField_AttestedCopy.widthAnchor.constraint(equalToConstant: 150).isActive = true
         
         Btn_AttestedCopy.translatesAutoresizingMaskIntoConstraints = false
-        Btn_AttestedCopy.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 200).isActive = true
+        Btn_AttestedCopy.leadingAnchor.constraint(equalTo: view.centerXAnchor, constant: 70).isActive = true
         Btn_AttestedCopy.centerYAnchor.constraint(equalTo: textField_AttestedCopy.centerYAnchor).isActive = true
         Btn_AttestedCopy.widthAnchor.constraint(equalToConstant: 25).isActive = true
         Btn_AttestedCopy.heightAnchor.constraint(equalTo: Btn_AttestedCopy.widthAnchor).isActive = true
         
         childInfo_txt1.translatesAutoresizingMaskIntoConstraints = false
-        
+        childInfo_txt1.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: -50).isActive = true
+        childInfo_txt1.widthAnchor.constraint(equalToConstant: 80).isActive = true
+        childInfo_txt1.topAnchor.constraint(equalTo: textField_AttestedCopy.bottomAnchor, constant: 20).isActive = true
     }
 }
