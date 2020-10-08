d5276cfd5f5944e85294aed4e47bc55a86bbbf4b
chagmn <sopg4@naver.com>
2020-10-08 12:25:42 +0900

    Add AddCustomer5 View

---
+++ Justy-UIKit/AddCustomer5.swift
@@ -0,0 +1,17 @@
+//
+//  AddCustomer5.swift
+//  Justy-UIKit
+//
+//  Created by 창민 on 2020/10/08.
+//  Copyright © 2020 창민. All rights reserved.
+//
+
+import UIKit
+
+class AddCustomer5: UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        overrideUserInterfaceStyle = .light
+    }
+}
