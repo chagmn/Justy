35f57b7a595155b8620c5f2affef2c3e205f4cb8
chagmn <sopg4@naver.com>
2020-10-08 19:04:47 +0900

    update AddCustomer4 View

--- Justy-UIKit/AddCustomer4.swift
+++ Justy-UIKit/AddCustomer4.swift
@@ -13,10 +13,10 @@ class AddCustomer4: UIViewController{
     let mainColor = #colorLiteral(red: 1, green: 0.8799968362, blue: 0.2822909951, alpha: 1)
     let screenWidth: CGFloat = UIScreen.main.bounds.size.width
     let screenHeight: CGFloat = UIScreen.main.bounds.size.height
+    let picker = UIImagePickerController()
 
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var text1: UILabel!
-    @IBOutlet weak var micBtn: UIButton!
     @IBOutlet weak var textField_AttestedCopy: UITextField!
     @IBOutlet weak var Btn_AttestedCopy: UIButton!
     @IBOutlet weak var childInfo_txt1: UILabel!
@@ -31,6 +31,7 @@ class AddCustomer4: UIViewController{
         super.viewDidLoad()
         overrideUserInterfaceStyle = .light
         autoLayout()
+        picker.delegate = self
        
         img.image = UIImage(systemName: "questionmark.circle.fill")
         img.tintColor = mainColor
@@ -40,27 +41,39 @@ class AddCustomer4: UIViewController{
         text1.textColor = .black
         text1.font = UIFont.boldSystemFont(ofSize: 22)
         
-        micBtn.setImage(UIImage.init(systemName: "mic.fill"), for: .normal)
-        micBtn.tintColor = .black
-        micBtn.setTitle("", for: .normal)
-        micBtn.contentVerticalAlignment = .fill
-        micBtn.contentHorizontalAlignment = .fill
         
         textField_AttestedCopy.isEnabled = false
+        textField_AttestedCopy.insertText("등본 업로드")
+        textField_AttestedCopy.textColor = .lightGray
         
         Btn_AttestedCopy.setTitle("", for: .normal)
         Btn_AttestedCopy.setImage(UIImage(systemName: "tray.and.arrow.up.fill"), for: .normal)
         Btn_AttestedCopy.tintColor = .black
         Btn_AttestedCopy.contentVerticalAlignment = .fill
         Btn_AttestedCopy.contentHorizontalAlignment = .fill
+        Btn_AttestedCopy.addTarget(self, action: #selector(UploadFile), for: .touchUpInside)
+        
+        let toolbar = UIToolbar()
+        toolbar.sizeToFit()
+        let doneBtn = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(click_doneBtn))
+        let flexiblespace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+        toolbar.items = [flexiblespace, doneBtn]
         
         childInfo_txt1.text = "자식 정보 :"
         childInfo_txt1.textAlignment = .center
         childInfo_txt1.textColor = .black
         
+        childInfo_boy.keyboardType = .numberPad
+        childInfo_boy.textAlignment = .center
+        childInfo_boy.inputAccessoryView = toolbar
+            
         childInfo_boy_txt.text = "남"
         childInfo_boy_txt.textAlignment = .center
         childInfo_boy_txt.textColor = .black
+        
+        childInfo_girl.keyboardType = .numberPad
+        childInfo_girl.textAlignment = .center
+        childInfo_girl.inputAccessoryView = toolbar
         
         childInfo_girl_txt.text = "여"
         childInfo_girl_txt.textAlignment = .center
@@ -77,11 +90,44 @@ class AddCustomer4: UIViewController{
         nextBtn.addTarget(self, action: #selector(nextview), for: .touchUpInside)
         
     }
+    
+    @objc func UploadFile(){
+        let alert = UIAlertController(title: "사진 업로드", message: "원하는 방법을 선택하시오.", preferredStyle: .actionSheet)
+        
+        let library = UIAlertAction(title: "사진앨범", style: .default){ (action) in self.openLibrary()
+        }
+        
+        let camera = UIAlertAction(title: "카메라", style: .default){ (action) in
+            self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .default, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func openCamera(){
+        picker.sourceType = .camera
+        present(picker, animated: false, completion: nil)
+    }
+    
     @objc func nextview(){
         if let controller = self.storyboard?.instantiateViewController(identifier: "AddCustomerLast"){
             controller.modalPresentationStyle = .currentContext
             self.present(controller, animated: false, completion: nil)
         }
+    }
+    
+    @objc func click_doneBtn(){
+        view.endEditing(true)
     }
     
     func autoLayout(){
@@ -96,14 +142,9 @@ class AddCustomer4: UIViewController{
         text1.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         text1.widthAnchor.constraint(equalToConstant: 300).isActive = true
         
-        micBtn.translatesAutoresizingMaskIntoConstraints = false
-        micBtn.topAnchor.constraint(equalTo: text1.bottomAnchor, constant: 15).isActive = true
-        micBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        micBtn.widthAnchor.constraint(equalToConstant: 30).isActive = true
-        micBtn.heightAnchor.constraint(equalToConstant: 35).isActive = true
         
         textField_AttestedCopy.translatesAutoresizingMaskIntoConstraints = false
-        textField_AttestedCopy.topAnchor.constraint(equalTo: micBtn.bottomAnchor, constant: 50).isActive = true
+        textField_AttestedCopy.topAnchor.constraint(equalTo: text1.bottomAnchor, constant: 150).isActive = true
         textField_AttestedCopy.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: 50).isActive = true
         textField_AttestedCopy.widthAnchor.constraint(equalToConstant: 150).isActive = true
         
@@ -142,4 +183,10 @@ class AddCustomer4: UIViewController{
         nextBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
         nextBtn.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
+}
+extension AddCustomer4:UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print(info)
+    }
+    
 }
