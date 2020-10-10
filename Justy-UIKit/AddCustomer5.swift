f541621115572009ac416c97b7bc1f8e4d6716c2
chagmn <sopg4@naver.com>
2020-10-10 21:38:02 +0900

    update AddCustomer5 View

--- Justy-UIKit/AddCustomer5.swift
+++ Justy-UIKit/AddCustomer5.swift
@@ -11,12 +11,13 @@ import UIKit
 // 필요 서류 등록
 class AddCustomer5: UIViewController{
     let mainColor = #colorLiteral(red: 1, green: 0.8799968362, blue: 0.2822909951, alpha: 1)
+    let picker = UIImagePickerController()
     
     @IBOutlet weak var img: UIImageView!
     @IBOutlet weak var text1: UILabel!
-    
     @IBOutlet weak var field1: UITextField!
     @IBOutlet weak var addBtn1: UIButton!
+    @IBOutlet weak var nextBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,7 +32,61 @@ class AddCustomer5: UIViewController{
         text1.textColor = .black
         text1.font = UIFont.boldSystemFont(ofSize: 22)
         
-        field1.placeholder = "ff"
+        field1.placeholder = "진단서 등록"
+        field1.textAlignment = .center
+        
+        addBtn1.setTitle("", for: .normal)
+        addBtn1.setImage(UIImage(systemName: "tray.and.arrow.up.fill"), for: .normal)
+        addBtn1.tintColor = .black
+        addBtn1.contentVerticalAlignment = .fill
+        addBtn1.contentHorizontalAlignment = .fill
+        addBtn1.addTarget(self, action: #selector(UploadFile), for: .touchUpInside)
+        
+        
+        nextBtn.setTitle("next>", for: .normal)
+        nextBtn.titleLabel?.font = UIFont.systemFont(ofSize: 21.0)
+        nextBtn.tintColor = .white
+        nextBtn.backgroundColor = mainColor
+        nextBtn.layer.borderWidth = 1
+        nextBtn.layer.cornerRadius = 20
+        nextBtn.clipsToBounds = true
+        nextBtn.layer.borderColor = UIColor.clear.cgColor
+        nextBtn.addTarget(self, action: #selector(nextview), for: .touchUpInside)
+    }
+    
+    @objc func nextview(){
+        if let controller = self.storyboard?.instantiateViewController(identifier: "AddCustomerLast"){
+            controller.modalPresentationStyle = .currentContext
+            self.present(controller, animated: false, completion: nil)
+        }
+    }
+    
+    @objc func UploadFile(){
+        let alert = UIAlertController(title: "사진 업로드", message: "원하는 방법을 선택하시오.", preferredStyle: .actionSheet)
+        
+        let library = UIAlertAction(title: "사진앨범", style: .default){ (action) in self.openLibrary()
+        }
+        
+        let camera = UIAlertAction(title: "카메라", style: .default){ (action) in
+            self.openCamera()
+        }
+        let cancel = UIAlertAction(title: "취소", style: .default, handler: nil)
+        
+        alert.addAction(library)
+        alert.addAction(camera)
+        alert.addAction(cancel)
+        
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func openLibrary(){
+        picker.sourceType = .photoLibrary
+        present(picker, animated: false, completion: nil)
+    }
+    
+    func openCamera(){
+        picker.sourceType = .camera
+        present(picker, animated: false, completion: nil)
     }
     
     func autoLayout(){
@@ -47,8 +102,26 @@ class AddCustomer5: UIViewController{
         text1.widthAnchor.constraint(equalToConstant: 300).isActive = true
         
         field1.translatesAutoresizingMaskIntoConstraints = false
-        field1.topAnchor.constraint(equalTo: text1.bottomAnchor, constant: 20).isActive = true
-        field1.trailingAnchor.constraint(equalTo: view.centerXAnchor, constant: -20).isActive = true
+        field1.topAnchor.constraint(equalTo: text1.bottomAnchor, constant: 25).isActive = true
+        field1.rightAnchor.constraint(equalTo: view.centerXAnchor, constant: 50).isActive = true
         field1.widthAnchor.constraint(equalToConstant: 150).isActive = true
+        
+        addBtn1.translatesAutoresizingMaskIntoConstraints = false
+        addBtn1.centerYAnchor.constraint(equalTo: field1.centerYAnchor).isActive = true
+        addBtn1.leftAnchor.constraint(equalTo: field1.rightAnchor, constant: 15).isActive = true
+        addBtn1.widthAnchor.constraint(equalToConstant: 25).isActive = true
+        addBtn1.heightAnchor.constraint(equalTo: addBtn1.widthAnchor).isActive = true
+        
+        nextBtn.translatesAutoresizingMaskIntoConstraints = false
+        nextBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        nextBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -20).isActive = true
+        nextBtn.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        nextBtn.heightAnchor.constraint(equalToConstant: 40).isActive = true
     }
+}
+extension AddCustomer5: UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        print(info)
+    }
+    
 }
