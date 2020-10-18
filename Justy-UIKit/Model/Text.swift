c5e5ecedd43fc469b0cb95d52e4ce66da24dfbf2
chagmn <sopg4@naver.com>
2020-10-18 19:36:52 +0900

    move ViewControllers in View Group

---
+++ Justy-UIKit/Model/Text.swift
@@ -0,0 +1,13 @@
+//
+//  Text.swift
+//  Justy-UIKit
+//
+//  Created by 창민 on 2020/10/18.
+//  Copyright © 2020 창민. All rights reserved.
+//
+
+import Foundation
+
+struct Text{
+    var text: String
+}
